4679adcb5f0d408139c2d38366484d1864f5d2be
anonymix007 <anonymix007@users.noreply.github.com>
2023-04-22 20:03:25 +0300

    Update kernel

--- snk.sh
+++ snk.sh
@@ -2,7 +2,7 @@
 qemu-system-x86_64 \
 	-m 2G \
 	-smp 2 \
-	-kernel linux-6.2.11/arch/x86/boot/bzImage \
+	-kernel linux-6.2.y/arch/x86/boot/bzImage \
 	-append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0" \
 	-drive file=debian-img/bullseye_snk.img,format=raw \
 	-net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10031-:22 \
